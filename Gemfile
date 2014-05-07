652ae323507732d524a25bcb882775af1bc7a4d2
Joey Wolf <halfdark000@gmail.com>
2014-05-07 12:35:23 +0100

    kinda sorta working

--- Gemfile
+++ Gemfile
@@ -4,8 +4,11 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'shotgun'
 gem 'sinatra'
-gem "cucumber"
-gem "capybara"
-gem "rspec"
-gem "cucumber-sinatra"
 gem "newrelic_rpm"
+
+group :development, :test do
+	gem "rspec"
+	gem "cucumber"
+	gem "capybara"
+	gem "cucumber-sinatra"
+end
--- server.rb
+++ server.rb
@@ -1,8 +1,10 @@
 env = ENV["RACK_ENV"] || "development"
+require "data_mapper"
+require "sinatra"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 require './lib/link.rb'
 DataMapper.finalize
-DataMapper.auto_update!
+# DataMapper.auto_update!
 
 configure :production do
   require 'newrelic_rpm'
