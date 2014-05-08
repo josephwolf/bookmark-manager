208885a1bedfbda9aa64d2b72d27dfc072d42065
Joey Wolf <halfdark000@gmail.com>
2014-05-08 12:21:49 +0100

    added functionality

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -4,11 +4,11 @@ feature "User browses the list of links" do
 
 	before(:each) {
 		Link.create(:url => "http://www.makeracademy.com",
-			:title => "Lemon Party")
+			:title => "Makers Academy")
 	}
 	
 	scenario "when opening the home page" do
 		visit '/'
-		expect(page).to have_content("Lemon Party")
+		expect(page).to have_content("Makers Academy")
 	end
 end
